5a87d4a0b8cb7c07fb932a8053cd7cb854a66aa2
Sam Heckle <samheckle@yahoo.com>
2018-01-31 16:36:12 -0500

    finished l system

---
+++ lsystems/app.js
@@ -0,0 +1,57 @@
+const rules = {
+    F: 'FF-[-F+F+F]+[+F-F-F]',
+    //B: 'ABA'
+}
+
+const axiom = 'F'
+
+let output = axiom
+
+let numGenerations = 4
+
+let deg = 22.5;
+let rad = deg * Math.PI / 180
+
+
+var canvas = document.getElementsByTagName('canvas')[0]
+var ctx = canvas.getContext('2d')
+
+this.canvas.width = this.height = window.innerWidth
+this.canvas.height = this.width = window.innerHeight
+
+var turtle = Turtle.create(ctx, canvas.width / 2, canvas.height)
+
+for (let i = 0; i < numGenerations; i++) {
+    let newOutput = ''
+    for (let char of output) {
+        if (char == 'F') {
+            turtle.move(10)
+            newOutput = output.replace(/F/g, rules['F'])
+        } else if (char == '-') {
+            turtle.rotate(rad)
+        } else if (char == '+') {
+            turtle.rotate(-rad)
+        } else if (char == '[') {
+            turtle.push();
+        } else if (char == ']') {
+            turtle.pop();
+        }
+        // if (char == 'F')
+        //     newOutput = output.replace(/F/g, rules['F'])
+    }
+    output = newOutput
+}
+
+// for (let char of output) {
+//     if (char == 'F') {
+//         turtle.move(10)
+//     } else if (char == '-') {
+//         turtle.rotate(rad)
+//     } else if (char == '+') {
+//         turtle.rotate(-rad)
+//     } else if (char == '[') {
+//         turtle.push();
+//     } else if (char == ']') {
+//         turtle.pop();
+//     }
+// }
