e7d2671322bc4f5290e8c96e319ed4dc8c82c0c9
Sam Heckle <samheckle@yahoo.com>
2018-04-14 14:40:07 -0400

    added project 2

---
+++ project2/app.js
@@ -0,0 +1,11 @@
+// import our three.js reference
+const THREE = require('three')
+const PP = require('postprocessing')
+
+const app = {
+    init(){
+        
+    }
+}
+
+window.onload = () => app.init()
